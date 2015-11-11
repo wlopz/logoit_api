9796143f013fe13df9a0541696dd5c7223707097
Wilfredo Lopez <jrlopez85@gmail.com>
2015-11-11 17:25:03 -0500

    Improved website design and added images

--- server.rb
+++ server.rb
@@ -3,7 +3,6 @@ require 'unirest'
 require 'mini_magick'
 require 'json'
 require "sinatra/reloader" if development?
-
 require 'open-uri'
 
 get '/' do
@@ -30,15 +29,12 @@ post '/' do
 
 	response = Unirest.post 'https://search.craftar.net/v1/search',
 	        parameters: {
-            token: "703eb042371c49f0",
+            token: "ee40a93438eb49ee",
 	       		image: resized_tempfile				                        
 	        }
 
-	# if body == response
-
 		body = response.body
-	 
-	# p response.body
+
 	if body["error"]
 	 	code = body['error']['code']
 		message = body['error']['message']
@@ -54,16 +50,3 @@ post '/' do
 
 	# erb :show
 end
-
-# helpers do
-#   def scale_img
-#   	tempfile = Tempfile.open(filename, 'wb') { |fp| fp.write(response.body) }
-
-#   	user_image = MiniMagick::Image.open( tempfile.path )
-#     user_image.thumbnail( "600x400" )
-#     user_image.write( File.join )
-
-#   end
-# end
-
-# https://my.craftar.net/api/v0/image/?api_key=b44ec443f934a284c4a0b1bd204d168680027d62
